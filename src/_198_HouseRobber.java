ec9856e315d390cf582546bd377d53265d548686
EakonZhao <eakonzhao@gmail.com>
2017-06-05 00:50:56 +0800

    leetCode-solution

--- src/_198_HouseRobber.java
+++ src/_198_HouseRobber.java
@@ -18,10 +18,54 @@
  *  6[3,(4),1,(2)]
  *  6[(4,)3,1,(2)]
  *
- *  暴力解法:检查所有房子的组合，对每一个组合，检查是否有相邻的房子，如果没有，记录其价值。找最大值。时间复杂度为 O((2^n)*n)
+ *  暴力解法:检查所有房子的组合，对每一个组合，检查是否有相邻的房子，如果没有，记录其价值。
+ *  找最大值。时间复杂度为 O((2^n)*n)
  */
 public class _198_HouseRobber {
+
+    /**
+     * 记忆化搜索
+     * @param nums
+     * @return
+     */
     public int rob(int[] nums){
-        return 0;
+        if(nums==null||nums.length==0) return 0;
+        int[] dp = new int[nums.length];
+        for(int i=0; i<dp.length; i++){
+            dp[i]=-1;
+        }
+        return tryRob(nums,dp,0);
     }
+
+    private int tryRob(int[] nums, int[] dp, int index){
+        if(index>=nums.length) return 0;
+        if(dp[index]==-1){
+            int result=0;
+            for(int i=index; i<dp.length; i++){
+                result = Max3(result,dp[i],nums[i]+tryRob(nums,dp,index+2));
+            }
+            dp[index] = result;
+        }
+        return dp[index];
+    }
+    private int Max3(int a, int b, int c){
+        return  Math.max(a,Math.max(b,c));
+    }
+
+    /**
+     * 动态规划
+     * @param nums
+     * @return
+     */
+ /*   public int rob2(int[] nums){
+        if(nums==null||nums.length==0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j<n; j++){
+                dp[i] = Max3(dp[i],);
+            }
+        }
+    }*/
 }
