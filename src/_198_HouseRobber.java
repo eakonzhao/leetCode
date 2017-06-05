d816e340cb44d22e176db3a3b07fd473c7d20ab4
EakonZhao <eakonzhao@gmail.com>
2017-06-05 21:39:03 +0800

    leetCode-solution

--- src/_198_HouseRobber.java
+++ src/_198_HouseRobber.java
@@ -29,43 +29,42 @@ public class _198_HouseRobber {
      * @return
      */
     public int rob(int[] nums){
-        if(nums==null||nums.length==0) return 0;
-        int[] dp = new int[nums.length];
-        for(int i=0; i<dp.length; i++){
+        if(nums==null || nums.length==0) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        for(int i=0; i<n; i++){
             dp[i]=-1;
         }
         return tryRob(nums,dp,0);
     }
 
     private int tryRob(int[] nums, int[] dp, int index){
-        if(index>=nums.length) return 0;
+        if(index>=dp.length) return 0;
         if(dp[index]==-1){
-            int result=0;
+            int result= 0;
             for(int i=index; i<dp.length; i++){
-                result = Max3(result,dp[i],nums[i]+tryRob(nums,dp,index+2));
+               result =  Math.max(nums[i]+tryRob(nums,dp,index+2),result);
             }
             dp[index] = result;
         }
         return dp[index];
     }
-    private int Max3(int a, int b, int c){
-        return  Math.max(a,Math.max(b,c));
-    }
-
     /**
      * 动态规划
      * @param nums
      * @return
      */
- /*   public int rob2(int[] nums){
+    public int rob2(int[] nums){
         if(nums==null||nums.length==0) return 0;
         int n = nums.length;
         int[] dp = new int[n];
+        // dp[i]表示考虑抢劫 nums[i...n-1]所能获得的最大收益
         dp[n-1] = nums[n-1];
         for(int i=n-2; i>=0; i--){
-            for(int j=i; j<n; j++){
-                dp[i] = Max3(dp[i],);
+            //dp[i]
+            for(int j=i; j<n; j++)
+              dp[i] = Math.max(dp[i],nums[j]+(j+2<n?dp[j+2]:0));
             }
-        }
-    }*/
+            return dp[0];
+    }
 }
